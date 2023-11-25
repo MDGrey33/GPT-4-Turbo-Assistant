a4daf1fb93e03ae1adc624820e221c2ed1f71ab8
Roland Jones <roland@abouyounes.com>
2023-11-25 11:19:12 +0200

    Create file_manager.py
    file_manager module to host the file manager class

---
+++ file_manager.py
@@ -0,0 +1,17 @@
+
+class FileManager:
+    def __init__(self, client):
+        self.client = client.files
+
+    def create(self, file_path, purpose):
+        with open(file_path, 'rb') as file_object:
+            response = self.client.create(file=file_object, purpose=purpose)
+            return response.id
+
+    def list(self):
+        files_data = self.client.list().data
+        return {file.id: {"filename": file.filename, "purpose": file.purpose} for file in files_data}
+
+    def delete(self, file_id):
+        self.client.delete(file_id)
+        return f"File with ID {file_id} has been deleted."
